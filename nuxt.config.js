33a04ad68b7037e34a9e9c6adf90be4fa0784369
Arthur Henrique <arthur12souza@hotmail.com>
2023-01-26 11:24:14 -0300

    config eslint and prettier

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,36 +1,35 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "Braip Front-end Starter",
+    title: 'Braip Front-end Starter',
     htmlAttrs: {
-      lang: "pt-BR",
+      lang: 'pt-BR',
     },
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: "description",
-        name: "description",
-        content: "descrição",
+        hid: 'description',
+        name: 'description',
+        content: 'descrição',
       },
-      { name: "format-detection", content: "telephone=no" },
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.png" },
-      { rel: "stylesheet", href: "https://css.gg/css" },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+      { rel: 'stylesheet', href: 'https://css.gg/css' },
       {
-        rel: "stylesheet",
-        href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css",
+        rel: 'stylesheet',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css',
       },
     ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    "@/assets/css/sweetalert2.css",
-    "@/assets/css/fontawesome.css",
-    "@/assets/css/reset.css",
-    "@/assets/css/global.css",
+    '@/assets/css/fontawesome.css',
+    '@/assets/css/reset.css',
+    '@/assets/css/global.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -38,26 +37,26 @@ export default {
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: [
-    "@/components",
+    '@/components',
     {
-      path: "@components/templates/",
-      prefix: "template",
-      extensions: ["vue"],
+      path: '@components/templates/',
+      prefix: 'template',
+      extensions: ['vue'],
     },
     {
-      path: "@components/organisms/",
-      prefix: "organism",
-      extensions: ["vue"],
+      path: '@components/organisms/',
+      prefix: 'organism',
+      extensions: ['vue'],
     },
     {
-      path: "@components/molecule/",
-      prefix: "molecule",
-      extensions: ["vue"],
+      path: '@components/molecules/',
+      prefix: 'molecule',
+      extensions: ['vue'],
     },
     {
-      path: "@components/atoms/",
-      prefix: "atom",
-      extensions: ["vue"],
+      path: '@components/atoms/',
+      prefix: 'atom',
+      extensions: ['vue'],
     },
   ],
 
@@ -65,7 +64,7 @@ export default {
   buildModules: [],
 
   fontawesome: {
-    component: "fa",
+    component: 'fa',
     icons: {
       solid: true,
       brands: true,
@@ -73,12 +72,26 @@ export default {
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ['@nuxtjs/eslint-module'],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    extractCSS: true,
+    collapseBooleanAttributes: true,
+    decodeEntities: true,
+    minifyCSS: true,
+    minifyJS: true,
+    processConditionalComments: true,
+    removeEmptyAttributes: true,
+    removeRedundantAttributes: true,
+    trimCustomFragments: true,
+    useShortDoctype: true,
+    minifyURLs: true,
+    removeComments: true,
+    removeEmptyElements: true,
+  },
 
   env: {
     NODE_ENV: process.env.NODE_ENV,
   },
-};
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,42 @@
 <template>
-  <Tutorial />
+  <h1>hello world</h1>
 </template>
 
 <script>
 export default {
-  name: "IndexPage",
-};
+  name: 'IndexPage',
+
+  watch: {
+    data(newValue, oldValue) {},
+  },
+
+  methods: {
+    name(name) {
+      if (name.length > 30) {
+        return 'your name is too big'
+      } else if (name.length < 10) {
+        return 'your name is too short'
+      } else {
+        return 'your name is ok'
+      }
+    },
+  },
+  async fetch() {},
+
+  data() {
+    return {
+      my_var: true,
+    }
+  },
+
+  mounted() {
+    console.log('mounted')
+  },
+
+  computed: {
+    name() {
+      return this.data
+    },
+  },
+}
 </script>
